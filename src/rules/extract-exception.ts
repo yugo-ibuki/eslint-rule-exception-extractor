adb4e91299ac0cf14ec90d053d5131d5c4f179cf
yugo-ibuki <y.ibuki91@gmail.com>
2024-05-19 15:38:12 +0900

    Refactor rule extraction for throw statement in functions
    The function extraction rule in extract-exception.ts has been refactored for better readability and efficiency. This update focuses on the detection of 'ThrowStatement' within a function. Now instead of entering each node, it automatically traverses through child nodes, reporting an error when a throw statement is found.

--- src/rules/extract-exception.ts
+++ src/rules/extract-exception.ts
@@ -6,26 +6,38 @@ const throwStatement = '@throws'
 
 const rule: Rule.RuleModule = {
   create: (context) => {
-    return {
-      ':function': (node: Node) => {
+    function traverse(node: Node) {
+      if (node.type === 'ThrowStatement') {
+        context.report({
+          node: node,
+          message: 'Throw statement found within a function.',
+        })
+      }
+
+      if (
+        node.type === 'FunctionDeclaration' ||
+        node.type === 'FunctionExpression' ||
+        node.type === 'ArrowFunctionExpression'
+      ) {
         // check if the function has a throw statement
         const hasThrowStatement = context.sourceCode
           .getJSDocComment(node)
           ?.value.includes(throwStatement)
 
-        // If there is no throw statement, return
-        estraverse.traverse(node, {
-          enter(childNode) {
-            // If there is a throw statement and there is no throw statement,
-            // report an error
-            if (childNode.type === 'ThrowStatement' && !hasThrowStatement) {
-              context.report({
-                node: childNode,
-                message: 'Throw statement found within a function.',
-              })
-            }
-          },
-        })
+        // If there is no throw statement, do nothing
+        if (!hasThrowStatement) {
+          // If there is a throw statement and there is no throw statement,
+          // report an error
+          if (node.body.type === 'BlockStatement') {
+            node.body.body.forEach(traverse)
+          }
+        }
+      }
+    }
+
+    return {
+      ':function': (node: Node) => {
+        traverse(node)
       },
     }
   },
